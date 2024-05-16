be2c5d7e3ea093f08d76ba60402e32e4ef50e73d
Najumudeen Fathima Munasa <79048000+Munz10@users.noreply.github.com>
2024-05-16 16:23:53 +0530

    signup validations

--- CodeIgniter_3/application/views/signup.php
+++ CodeIgniter_3/application/views/signup.php
@@ -4,6 +4,11 @@
     <meta charset="UTF-8">
     <title>Signup - VividSpace</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js" type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.8.3/underscore-min.js"
+        type="text/javascript"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.3/backbone-min.js"
+        type="text/javascript"></script>
     <style>
         body {
             font-family: 'Roboto', sans-serif; /* Set default font */
@@ -59,10 +64,12 @@
 <body>
     <div class="container  mt-5">
         <h1>VividSpace</h1>
-        <form method="post" id="signupForm">
+        <form method="post" id="signupForm" name="signupform">
+        <div class="errormsg" id="errormsg"></div>
             <div class="form-group">
                 <label for="username">Username<span class="required"></span></label>
-                <input type="text" class="form-control" name="username" id="username" placeholder="Username" required>
+                <input type="text" class="form-control" name="username" id="username" placeholder="Username"
+                onkeyup='checkusername(); checkinputs();' required />
             </div>
             <div class="form-group">
                 <label for="first_name">First Name</label>
@@ -74,48 +81,95 @@
             </div>
             <div class="form-group">
                 <label for="email">Email<span class="required"></span></label>
-                <input type="email" class="form-control" name="email" id="email" placeholder="Email" required>
+                <input type="email" class="form-control" name="email" id="email" placeholder="Email" 
+                onkeyup='checkinputs(); validateemail()' required />
             </div>
             <div class="form-group">
                 <label for="password">Password<span class="required"></span></label>
                 <input type="password" class="form-control" name="password" id="password" placeholder="Password" required>
             </div>
-            <button type="submit" class="btn btn-dark">Signup</button>
+            <button type="submit" id= createUser class="btn btn-dark" disabled="disabled">Signup</button>
             <p class="mt-3"> Have an account? <a href="<?= site_url('login'); ?>" > Login</a></p>
         </form>
     </div>
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/backbone.js/1.2.3/backbone-min.js"></script>
-    <script>
-        var UserModel = Backbone.Model.extend({
-            urlRoot: '<?= site_url('signup/index_post'); ?>'
-        });
 
-        $('#signupForm').submit(function(event) {
-            event.preventDefault();
-
-            var userData = {
-                username: $('input[name="username"]').val(),
-                first_name: $('input[name="first_name"]').val(),
-                last_name: $('input[name="last_name"]').val(),
-                email: $('input[name="email"]').val(),
-                password: $('input[name="password"]').val()
-            };
-
-            var newUser = new UserModel(userData);
-
-            newUser.save(null, {
-                success: function(model, response) {
-                    console.log('Signup successful:', response);
-                    // Redirect to login page
-                    window.location.href = 'login.php';
-                },
-                error: function(model, response) {
-                    console.error('Signup error:', response);
+    <script type="text/javascript" lang="javascript">
+        //check if required inputs are empty
+        function checkinputs() {
+            if (document.forms["signupform"]["username"].value != "" && document.forms["signupform"]["email"].value != "" &&
+                document.forms["signupform"]["password"].value != "" && document.getElementById("errormsg").innerHTML == "") {
+                document.getElementById('createUser').disabled = false;
+            }
+            else{
+                document.getElementById('createUser').disabled = true;
+            }
+        }
+        function validateemail() {//valid email before sending through api
+            var x = document.forms["signupform"]["email"].value;
+            var atposition = x.indexOf("@");
+            var dotposition = x.lastIndexOf(".");
+            if (atposition < 1 || dotposition < atposition + 2 || dotposition + 2 >= x.length) {
+                document.getElementById("errormsg").innerHTML = "Please enter a valid e-mail address";
+            }
+            else {
+                document.getElementById("errormsg").innerHTML = "";
+                checkinputs();
+            }
+        }
+        function checkusername() {//check if the username is taken or not
+            $.ajax({
+                url: "<?php echo base_url() ?>index.php/signup/check_user",
+                data: { 'username': "@" + $('#username').val().toLowerCase() },
+                method: "POST"
+            }).done(function (data) {
+                if (data == 0) {
+                    document.getElementById("errormsg").innerHTML = "";
+                    checkinputs();
+                }
+                else {
+                    document.getElementById("errormsg").innerHTML = "Username Already Exists!"
                 }
             });
+        }
+        $(document).ready(function () {
+            $('#createUser').click(function (event) {//call function when button is clicked
+                event.preventDefault();
+                userSignup();
+            });
         });
-    </script>
+        var User = Backbone.Model.extend({
+            urlRoot: '<?= site_url('signup/index_post'); ?>'
+        });
+        var UserCollection = Backbone.Collection.extend({
+            model: User
+        });
+        var usersCollection = new UserCollection();
+        function userSignup() {
+            var newUser = new User();
+            newUser.set('username', "@" + $("#username").val().toLowerCase());
+            newUser.set('email', $("#email").val());
+            newUser.set('first_name', $("#first_name").val());
+            newUser.set('last_name', $("#last_name").val());
+            newUser.set('password', $("#password").val());
+            usersCollection.create(newUser, {
+                success: function (model, response) {
+                    if (response.success) {
+                        // Redirect to the specified URL
+                        window.location.href = response.redirect_url;
+                    } else {
+                        // Handle the case where user creation fails
+                        // You can display an error message or take any other appropriate action
+                        console.error('Failed to create user:', response.error);
+                    }
+                },
+                error: function (model, xhr, options) {
+                    // Handle AJAX errors here
+                    console.error('AJAX error:', xhr.statusText);
+                }
+            });
+        }
+        </script>
 </body>
 </html>
